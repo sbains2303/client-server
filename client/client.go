15340fa7dadf27527aa5613f7497cf603da574d5
Saihajpreet Bains <123552495+sbains2303@users.noreply.github.com>
2023-10-20 09:03:31 +0100

    Update client.go

--- client/client.go
+++ client/client.go
@@ -1,40 +1,25 @@
 package main
 
 import (
-	"bufio"
-	"flag"
+	"flag" // for command line arguments
 	"fmt"
-	"net"
-	"os"
+	"net/rpc"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 )
 
-func read(conn net.Conn) {
-	//TODO In a continuous loop, read a message from the server and display it.
-	reader := bufio.NewReader(conn)
-	for {
-		msg, _ := reader.ReadString('\n')
-		fmt.Println(msg)
-	}
-}
-
-func write(conn net.Conn) {
-	//TODO Continually get input from the user and send messages to the server.
-	stdin := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Printf("Enter text->")
-		msg, _ := stdin.ReadString('\n')
-		fmt.Fprintln(conn, msg)
-	}
-}
-
 func main() {
-	// Get the server address and port from the commandline arguments.
-	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
+	// Parse CLI to get IP and port of server to connect to
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
-	//TODO Try to connect to the server
-	conn, _ := net.Dial("tcp", *addrPtr)
-	//TODO Start asynchronously reading and displaying messages
-	go read(conn)
-	//TODO Start getting and sending user messages.
-	go write(conn)
+	fmt.Println("Server: ", *server)
+	// Dial RPC server using provided server address
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+
+	request := stubs.Request{Message: "Hello"}
+	// Initialise response structure
+	response := new(stubs.Response)
+	client.Call(stubs.PremiumReverseHandler, request, response)
+	fmt.Println("Responded: " + response.Message)
 }
+
